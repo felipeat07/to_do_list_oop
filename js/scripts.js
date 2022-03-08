84602f2968e0f59aa974aafbe7e67a4e00657f97
Felipe Tavares <felipeat07@gmail.com>
2022-03-08 23:06:35 +0000

    implementando a funcao completar tarefas e texto de vazio

--- js/scripts.js
+++ js/scripts.js
@@ -19,8 +19,67 @@ addTask(taskText){
     let list = document.querySelector('#task-container');
 
     //inserindo na lista
-
     list.appendChild(template);
+
+    //adiciona evento as tasks
+    this.addEvents();
+
+    this.checkTasks('add');
+
+    }
+
+    removeTask(task){
+        //selecionando o elemento pai
+        let parentEl = task.parentElement;
+
+        //deletando o elemento
+        parentEl.remove();
+
+        this.checkTasks('remove');
+    }
+
+    completeTask(task){
+        //adicionando a classe done
+        task.classList.add('done');
+    }
+
+    addEvents(){
+
+        let removeBtns = document.querySelectorAll('.fa-trash');
+        let removeBtn = removeBtns[removeBtns.length - 1];
+
+        let doneBtns = document.querySelectorAll('.fa-check');
+        let doneBtn = doneBtns[doneBtns.length - 1];
+
+        //adicionando evento de remover
+            removeBtn.addEventListener('click', function(){
+            todo.removeTask(this);
+        });
+
+        //adicionando evento de completar tarefa
+        doneBtn.addEventListener('click', function(){
+            todo.completeTask(this);
+        });
+
+    }
+
+    checkTasks(command){
+
+        let msg = document.querySelector('#empty-tasks');
+
+        //logica para contar tasks adicionadas
+        if(command === 'add'){
+            this.totalTasks +=1;
+        } else if(command === 'remove'){
+            this.totalTasks -=1;
+        }
+
+        //verifica o contador de task para remover ou não msg
+        if(this.totalTasks == 1){
+            msg.classList.remove('hide');
+        } else {
+            msg.classList.add('hide');
+        }
 
     }
 
@@ -42,8 +101,8 @@ addBtn.addEventListener('click', function(e){
         todo.addTask(taskText.value);
     }
     
+    
     //limpa o campo de texto
-
     taskText.value = '';
 
 });
